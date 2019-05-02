e8e82f1410b999b581e8efbd92ff4974bebf6ffb
Thiago Morais <thiagomorais@projetosthiago.com.br>
2019-05-01 21:23:04 -0300

    commit aula 14

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const express = require("express"),
-  app = express(),
-  nunjucks = require("nunjucks"),
-  users = ["Thiago Morais", "Hugo Tostes", "Dario Silva"];
+const express = require('express')
+const app = express()
+const nunjucks = require('nunjucks')
+const users = ['Thiago Morais', 'Hugo Tostes', 'Dario Silva']
 
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
-});
+})
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 
 // app.use(express.json());
 
-app.set("view engine", "njk");
+app.set('view engine', 'njk')
 
-app.get("/", (req, res) => {
-  return res.render("list", { users });
-});
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
 
-app.get("/new", (req, res) => {
-  return res.render("new");
-});
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
 
-app.post("/create", (req, res) => {
-  users.push(req.body.user);
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
 
-  return res.redirect("/");
-});
-
-app.listen(3000);
+app.listen(3000)
